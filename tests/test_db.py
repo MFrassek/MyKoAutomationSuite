681d82f8acf4402aa55b91b57cb833008084c3bf
Martin <martinfrassek@gmx.de>
2020-10-21 17:48:07 +0200

    Initialize test class for running of pytest

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,14 +4,15 @@ from utils import init_db
 
 
 class TestInitiation(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestInitiation, self).__init__(*args, **kwargs)
+
     def setUp(self):
         self.data_path = "{}/test_data".format(
             os.path.dirname(os.path.abspath(__file__)))
 
     def tearDown(self):
         pass
-
-
 
 
 if __name__ == '__main__':
