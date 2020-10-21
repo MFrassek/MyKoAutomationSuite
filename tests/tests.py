a083a03927df5f68d642e946f6a286a9704fd96a
Martin <martinfrassek@gmx.de>
2020-10-21 11:46:53 +0200

    Add tests folder and begin writing testing class

---
+++ tests/tests.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class TestInitiation(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_find_all_weekend_file(self):
+        pass
+
+    def test_init_tables(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
