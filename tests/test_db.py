6cdd8479c0434e776d06c2be8b45eacf900a1505
Martin <martinfrassek@gmx.de>
2020-10-21 21:23:16 +0200

    Merge init tests together to reduce unneeded building of the DB

--- tests/test_db.py
+++ tests/test_db.py
@@ -26,12 +26,10 @@ class TestInitiation(unittest.TestCase):
     def test_connects_to_db(self):
         self.assertIsInstance(self.conn, sqlite3.Connection)
 
-    def test_no_old_tables_left_after_table_drop(self):
+    def test_initilization_components_successful(self):
         init_db.drop_old_tables(self.c)
         self.c.execute("SELECT name FROM sqlite_master WHERE type='table';")
         self.assertEqual(self.c.fetchall(), [])
-
-    def test_make_new_tables(self):
         init_db.create_all_tables(self.c)
         self.c.execute("SELECT name FROM sqlite_master WHERE type='table';")
         self.assertEqual(
