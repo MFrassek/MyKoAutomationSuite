c06088b4a72337fbe0dce27160c240fbf32a8028
Martin <martinfrassek@gmx.de>
2020-10-21 18:41:13 +0200

    Move setting ot self.data_path from setUp() to __init__()

--- tests/test_db.py
+++ tests/test_db.py
@@ -6,10 +6,11 @@ from utils import init_db
 class TestInitiation(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestInitiation, self).__init__(*args, **kwargs)
-
-    def setUp(self):
         self.data_path = "{}/test_data".format(
             os.path.dirname(os.path.abspath(__file__)))
+
+    def setUp(self):
+        pass
 
     def tearDown(self):
         pass
