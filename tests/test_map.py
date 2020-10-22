cc45613920ec48e4c3b7079397982302ea5b2080
Martin <martinfrassek@gmx.de>
2020-10-22 12:02:20 +0200

    Add test for generation of PNG

--- tests/test_map.py
+++ tests/test_map.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+from utils import mysec_map
 
 
 class TestInitiation(unittest.TestCase):
@@ -7,3 +8,13 @@ class TestInitiation(unittest.TestCase):
         super(TestInitiation, self).__init__(*args, **kwargs)
         self.data_path = "{}/test_data".format(
             os.path.dirname(os.path.abspath(__file__)))
+
+    def tearDown(self):
+        os.remove("{}/test_map.png".format(self.data_path))
+
+    def test_make_png(self):
+        mysec_map.generate_mysec_map(
+            self.data_path, "{}/test_map.png".format(self.data_path))
+        self.assertTrue(
+            os.path.exists("{}/test_map.png".format(self.data_path)),
+            "MYSec_Map.png does not exist at expected location")
