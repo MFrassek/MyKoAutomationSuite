e09043723b0d829eb271ae315ee15af6c006b548
Martin <martinfrassek@gmx.de>
2020-10-22 11:49:47 +0200

    Write base structure for testing mysec_map.py()

---
+++ tests/test_map.py
@@ -0,0 +1,9 @@
+import unittest
+import os
+
+
+class TestInitiation(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestInitiation, self).__init__(*args, **kwargs)
+        self.data_path = "{}/test_data".format(
+            os.path.dirname(os.path.abspath(__file__)))
