63a375d6f6c2506e92e4a9881bacadafe57a4748
Martin <martinfrassek@gmx.de>
2020-10-22 11:45:37 +0200

    Add clause to execute only when run directly, and correct path to LecSecRegions.svg

--- utils/mysec_map.py
+++ utils/mysec_map.py
@@ -1,11 +1,18 @@
 from cairosvg import svg2png
+import os
 
 
-def generate_mysec_map():
-    with open("LocSecRegions.svg", "r") as template_svg:
+def generate_mysec_map(data_path):
+    with open("{}/LocSecRegions.svg".format(data_path), "r") as template_svg:
         svg_code = "".join(template_svg.readlines())
         print(svg_code)
         svg2png(bytestring=svg_code, write_to='MYSec_map.png', dpi=300)
 
 
-generate_mysec_map()
+def get_relative_path_to_script():
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+if __name__ == '__main__':
+    data_path = "{}/data".format(get_relative_path_to_script())
+    generate_mysec_map(data_path)
