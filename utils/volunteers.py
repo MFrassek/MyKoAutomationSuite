834e644a8227c58bba8c05f58289c82811425f35
Martin <martinfrassek@gmx.de>
2020-10-24 00:09:05 +0200

    Add add_entry_to_table_x() to add MYSecs or MY-Veranstalter to their respective table

--- utils/volunteers.py
+++ utils/volunteers.py
@@ -3,12 +3,20 @@ import sqlite3
 
 def add_new_volunteer_and_position(db_name):
     conn, c = connect_to_db(db_name)
-    position_name = input("Position:\n")
+    position_names = input(
+        "Comma separated positions [mysecs, myvers]:\n").split(",")
     volunteerName = input("Name:\n")
     gender = input("Gender [f, m or d]:\n")
     birthDate = input("Birthdate [YYYY-MM-DD]:\n")
     add_entry_to_table_volunteers(c, volunteerName, gender, birthDate)
+    for position_name in position_names:
+        add_entry_to_table_x(c, position_name, volunteerName)
     deconnect_from_db(conn)
+
+
+def connect_to_db(db_name):
+    conn = sqlite3.connect(db_name)
+    return conn, conn.cursor()
 
 
 def add_entry_to_table_volunteers(c, volunteerName, gender, birthDate):
@@ -17,9 +25,12 @@ def add_entry_to_table_volunteers(c, volunteerName, gender, birthDate):
         .format(volunteerName, gender, birthDate))
 
 
-def connect_to_db(db_name):
-    conn = sqlite3.connect(db_name)
-    return conn, conn.cursor()
+def add_entry_to_table_x(c, table, volunteerName):
+    regionName = input("Region name:\n")
+    startDate = input("Start date [YYYY-MM-DD]:\n")
+    c.execute(
+        "INSERT INTO {} VALUES ('{}', '{}', '{}', '');"
+        .format(table, volunteerName, regionName, startDate))
 
 
 def deconnect_from_db(conn):
