e6eeabe8d64f75e085ae13bcffce6339dbf64383
Martin <martinfrassek@gmx.de>
2020-10-23 23:49:14 +0200

    Add section ofprompting for different variables to add_new_volunteer_and_position() and write add_entry_to_table_volunteers()

--- utils/volunteers.py
+++ utils/volunteers.py
@@ -4,7 +4,17 @@ import sqlite3
 def add_new_volunteer_and_position(db_name):
     conn, c = connect_to_db(db_name)
     position_name = input("Position:\n")
+    volunteerName = input("Name:\n")
+    gender = input("Gender [f, m or d]:\n")
+    birthDate = input("Birthdate [YYYY-MM-DD]:\n")
+    add_entry_to_table_volunteers(c, volunteerName, gender, birthDate)
     deconnect_from_db(conn)
+
+
+def add_entry_to_table_volunteers(c, volunteerName, gender, birthDate):
+    c.execute(
+        "INSERT INTO volunteers VALUES ('{}', '{}', '{}')"
+        .format(volunteerName, gender, birthDate))
 
 
 def connect_to_db(db_name):
