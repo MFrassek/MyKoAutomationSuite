f3aa0eb2a4c37cf0796ee9bbf298c2a0c0d35d1e
Martin <martinfrassek@gmx.de>
2020-11-14 23:46:49 +0100

    Add update_details() and update_value_in_table() to update some details by prompting for new values

--- utils/volunteer_update_details.py
+++ utils/volunteer_update_details.py
@@ -2,6 +2,22 @@ from helper import connect_to_db, disconnect_from_db,\
     get_general_volunteer_details, get_position_details
 
 
+def update_details(db_name):
+    conn, c = connect_to_db(db_name)
+    volunteerName = input("Name:\n")
+    all_positions = get_all_positions(c, volunteerName)
+    if all_positions:
+        _, gender, birthDate = get_general_volunteer_details(c, volunteerName)
+        update_value_in_table(c, volunteerName, "volunteers", "gender", gender)
+        update_value_in_table(c, volunteerName, "volunteers", "birthDate", birthDate)
+        for position in all_positions:
+            _, regionId, startDate, endDate = get_position_details(
+                c, volunteerName, position)
+    else:
+        print("No positions found for this name")
+    disconnect_from_db(conn)
+
+
 def get_all_positions(c, volunteerName):
     c.execute("SELECT name FROM sqlite_master WHERE type='table';")
     all_positions = []
@@ -14,3 +30,16 @@ def get_all_positions(c, volunteerName):
                 print(table[0])
                 all_positions.append(table[0])
     return all_positions
+
+
+def update_value_in_table(c, volunteerName, table, column_name, value):
+    new_value = input(
+        f"Current {column_name}: {value}\nNew {column_name}: ")
+    if new_value:
+        c.execute(f"""UPDATE {table}
+            SET {column_name} = '{new_value}'
+            WHERE volunteerName = '{volunteerName}';""")
+
+
+if __name__ == '__main__':
+    update_details("MY-Ko.db")
