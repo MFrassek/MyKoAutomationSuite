134e6587901480725f2b0ece54868d0f34b38278
Martin <martinfrassek@gmx.de>
2020-11-15 21:55:49 +0100

    Add tests for update_volunteer_details() and update_position_details()

--- tests/test_update.py
+++ tests/test_update.py
@@ -16,6 +16,7 @@ class TestInitiation(unittest.TestCase):
         self.conn, self.c = helper.connect_to_db(self.db_name)
 
     def tearDown(self):
+        volunteer_update_details.input = input
         helper.uncommited_disconnect_from_db(self.conn)
 
     def test_get_all_positions(self):
@@ -23,11 +24,28 @@ class TestInitiation(unittest.TestCase):
             volunteer_update_details.get_all_positions(self.c, "Test Name")
         self.assertEqual(all_positions, ["mysecs"])
 
+    def test_update_volunteer_details(self):
+        generator = (ele for ele in ["f", "1990-04-05"])
+        volunteer_update_details.input = lambda x: next(generator)
+        volunteer_update_details.update_volunteer_details(self.c, "Test Name")
+        self.c.execute("""SELECT gender, birthDate FROM volunteers
+                          WHERE volunteerName = 'Test Name'""")
+        self.assertEqual(self.c.fetchall()[0], ("f", "1990-04-05"))
+
+    def test_update_position_details(self):
+        generator = (ele for ele in ["Kiel", "2019-03-02", "2020-03-02"])
+        volunteer_update_details.input = lambda x: next(generator)
+        volunteer_update_details.update_position_details(
+            self.c, "Test Name", "mysecs")
+        self.c.execute("""SELECT regionName, startDate, endDate FROM mysecs
+                          WHERE volunteerName = 'Test Name'""")
+        self.assertEqual(
+            self.c.fetchall()[0], ("Kiel", "2019-03-02", "2020-03-02"))
+
     def test_update_value_in_table_without_input(self):
         volunteer_update_details.input = lambda x: ""
         volunteer_update_details.update_value_in_table(
             self.c, "Test Name", "mysecs", "startDate", "")
-        volunteer_update_details.input = input
         self.c.execute("""SELECT startDate FROM mysecs
                           WHERE volunteerName = 'Test Name'""")
         self.assertEqual(self.c.fetchall()[0], ("2020-10-10",))
@@ -36,7 +54,6 @@ class TestInitiation(unittest.TestCase):
         volunteer_update_details.input = lambda x: "2020-11-11"
         volunteer_update_details.update_value_in_table(
             self.c, "Test Name", "mysecs", "endDate", "")
-        volunteer_update_details.input = input
         self.c.execute("""SELECT endDate FROM mysecs
                           WHERE volunteerName = 'Test Name'""")
         self.assertEqual(self.c.fetchall()[0], ("2020-11-11",))
