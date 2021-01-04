80885e56de2a0872c7e27d067cc158dc2cfac502
Martin <martinfrassek@gmx.de>
2021-01-04 23:10:10 +0100

    Add getters for id_, name, mail_name and looking_state as well as setter for looking_state of Region

--- utils/region.py
+++ utils/region.py
@@ -4,3 +4,25 @@ class Region():
     self._name = name
     self._mail_name = mail_name
     self._looking_state = looking_state
+
+    @property
+    def id_(self):
+        return self._id_
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def mail_name(self):
+        return self._mail_name
+
+    @property
+    def looking_state(self):
+        return self._looking_state
+
+    @looking_state.setter
+    def looking_state(self, looking_state):
+        assert isinstance(looking_state, bool), \
+            "'looking_state' must be of type bool"
+        self._looking_state = looking_state
