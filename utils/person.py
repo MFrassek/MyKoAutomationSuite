6d888a07c8e44218d9a5a3d9349e941cb10a9a61
Martin <martinfrassek@gmx.de>
2021-01-04 17:12:36 +0100

    Add getters for name, birth_date, gender and region as well as a setter for region

--- utils/person.py
+++ utils/person.py
@@ -8,3 +8,25 @@ class Person:
     def __str__(self):
         return f"{self._name}, born: {self._birth_date}, " \
             + f"gender: {self._gender}, region: {self._region}"
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def birth_date(self):
+        return self._birth_date
+
+    @property
+    def gender(self):
+        return self._gender
+
+    @property
+    def region(self):
+        return self._region
+
+    @region.setter
+    def region(self, region):
+        assert isinstance(region, str), \
+            "'region' can only be set to type string."
+        self._region = region
