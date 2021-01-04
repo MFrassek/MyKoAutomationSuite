d92d0768d87f36dc89d41cdfd1939e14d90cfd30
Martin <martinfrassek@gmx.de>
2021-01-04 23:07:22 +0100

    Write Region class to represent entries of the region table

---
+++ utils/region.py
@@ -0,0 +1,6 @@
+class Region():
+    def __init__(self, id_: int, name: str, mail_name: str, looking_state: bool)
+    self._id_ = id_
+    self._name = name
+    self._mail_name = mail_name
+    self._looking_state = looking_state
