de120486f4d6cd132b7f2b2a015e1d1328c24045
Martin <martinfrassek@gmx.de>
2021-01-04 17:06:16 +0100

    Make Person class

---
+++ utils/person.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, name: str, birth_date: str, gender: str, region: str):
+        self._name = name
+        self._birth_date = birth_date
+        self._gender = gender
+        self._region = region
