e8e0ca5dced27a6a8c65bba981faa2b5d46c908d
Martin <martinfrassek@gmx.de>
2021-01-04 23:19:33 +0100

    Add factory method create_region()

--- utils/region.py
+++ utils/region.py
@@ -1,4 +1,9 @@
+from helper import connect_to_db, disconnect_from_db
+
+
 class Region():
+    db_name = "MY-Ko.db"
+
     def __init__(
             self, id_: int, name: str, mail_name: str, looking_state: bool):
         self._id_ = id_
@@ -30,3 +35,14 @@ class Region():
         assert isinstance(looking_state, bool), \
             "'looking_state' must be of type bool"
         self._looking_state = looking_state
+
+    @staticmethod
+    def create_region(name: str):
+        conn, c = connect_to_db(Region.db_name)
+        c.execute(
+            f"""SELECT *
+            FROM regions
+            WHERE regionName = '{name}'""")
+        data_tuple = c.fetchall()[0]
+        disconnect_from_db(conn)
+        return Region(*data_tuple)
