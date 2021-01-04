366e141286691aeeeec6b14e2679eb2a609f616a
Martin <martinfrassek@gmx.de>
2021-01-04 18:17:16 +0100

    Write factory method to create all volunteers fitting a given set of keyword arguments
    Write static method create_all_volunteers_fitting_data() and its helper
    functions create_volunteer_from_db_data_tuple(),
    get_volunteer_details_fitting_data(), and kwarg_to_column_name() similar
    to the respective functions in Position.
    Remove region as a parameter from __init__() as it is not contained in the DB
    and can therefore not be taken for initialization.

--- utils/person.py
+++ utils/person.py
@@ -1,16 +1,16 @@
 from position import Position
+from helper import connect_to_db, disconnect_from_db
 
 
 class Person:
-    def __init__(self, name: str, birth_date: str, gender: str, region: str):
+    def __init__(self, name: str, gender: str, birth_date: str):
         self._name = name
-        self._birth_date = birth_date
         self._gender = gender
-        self._region = region
+        self._birth_date = birth_date
 
     def __repr__(self):
-        return f"{self._name}, born: {self._birth_date}, " \
-            + f"gender: {self._gender}, region: {self._region}"
+        return f"{self._name}, gender: {self._gender}, " \
+            + f"born: {self._birth_date}"
 
     @property
     def name(self):
@@ -24,23 +24,12 @@ class Person:
     def gender(self):
         return self._gender
 
-    @property
-    def region(self):
-        return self._region
-
-    @region.setter
-    def region(self, region):
-        assert isinstance(region, str), \
-            "'region' can only be set to type string."
-        self._region = region
-
 
 class Volunteer(Person):
     def __init__(
-            self, name: str, birth_date: str, gender: str,
-            region: str):
+            self, name: str, birth_date: str, gender: str):
         super(Volunteer, self).__init__(
-            name, birth_date, gender, region)
+            name, birth_date, gender)
         self._positions = self.get_held_positions()
 
     def __repr__(self):
@@ -57,3 +46,39 @@ class Volunteer(Person):
             positions.extend(Position.create_all_positions_fitting_data(
                 title, held_by=self._name))
         return tuple(positions)
+
+    @staticmethod
+    def create_all_volunteers_fitting_data(**kwargs):
+        conn, c = connect_to_db(Position.db_name)
+        volunteers = [
+            Volunteer.create_volunteer_from_db_data_tuple(data_tuple)
+            for data_tuple
+            in Volunteer.get_volunteer_details_fitting_data(c, **kwargs)]
+        disconnect_from_db(conn)
+        return volunteers
+
+    @staticmethod
+    def create_volunteer_from_db_data_tuple(data_tuple: tuple):
+        name, birth_date, gender = data_tuple
+        return Volunteer(name, birth_date, gender)
+
+    @staticmethod
+    def get_volunteer_details_fitting_data(c, **kwargs):
+        assert len(kwargs) > 0, \
+            "At least one specifying key word argument must be given"
+        c.execute(
+            """SELECT *
+            FROM volunteers
+            WHERE """
+            + " AND ".join(
+             [Volunteer.kwarg_to_column_name(kwarg) + f" = '{kwargs[kwarg]}'"
+              for kwarg in kwargs.keys()]))
+        return c.fetchall()
+
+    @staticmethod
+    def kwarg_to_column_name(kwarg: str):
+        kwarg_to_column_name = {
+            "name": "volunteerName",
+            "birth_date": "birthDate",
+            "gender": "gender"}
+        return kwarg_to_column_name[kwarg]
