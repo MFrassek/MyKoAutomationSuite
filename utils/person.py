803b752e49955cbd6774c09827f8a6d48728b6c0
Martin <martinfrassek@gmx.de>
2021-01-04 17:09:31 +0100

    Add string representation to Person

--- utils/person.py
+++ utils/person.py
@@ -4,3 +4,7 @@ class Person:
         self._birth_date = birth_date
         self._gender = gender
         self._region = region
+
+    def __str__(self):
+        return f"{self._name}, born: {self._birth_date}, " \
+            + f"gender: {self._gender}, region: {self._region}"
