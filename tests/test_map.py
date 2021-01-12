b10688dc7f7eb842c17b7842bf16db0830f9b899
Martin <martinfrassek@gmx.de>
2021-01-12 23:32:40 +0100

    Decople tests from DB. Delete tests based on Region. Simplify test_make_png()

--- tests/test_map.py
+++ tests/test_map.py
@@ -1,7 +1,6 @@
 import unittest
 import os
 from utils import mysec_map
-from utils import helper
 from bs4 import BeautifulSoup
 
 
@@ -14,26 +13,21 @@ class TestInitiation(unittest.TestCase):
 
     def setUp(self):
         mysec_map.Region.db_name = self.db_name
-        self.conn, self.c = helper.connect_to_db(self.db_name)
 
     def tearDown(self):
-        helper.uncommited_disconnect_from_db(self.conn)
+        pass
 
     def test_make_png(self):
-        soup = mysec_map.get_wellformed_soup_from_svg_file(self.data_path)
-        mysec_map.make_png_from_soup(
-            soup, "{}/test_map.png".format(self.data_path))
+        functionTemp = mysec_map.prompt_region_ids_for_looking_state_change
+        mysec_map.prompt_region_ids_for_looking_state_change = lambda: []
+        print(self.db_name)
+        mysec_map.generate_mysec_map(
+            self.data_path, f"{self.data_path}/test_map.png")
         self.assertTrue(
-            os.path.exists("{}/test_map.png".format(self.data_path)),
+            os.path.exists(f"{self.data_path}/test_map.png"),
             "test_Map.png does not exist at expected location")
-        os.remove("{}/test_map.png".format(self.data_path))
-
-    def test_toggle(self):
-        conn, c = mysec_map.connect_to_db(self.db_name)
-        original_looking_state = mysec_map.Region.create_region_by_id("1020").looking_state
-        mysec_map.toggle_looking_state_in_db("1020")
-        new_looking_state = mysec_map.Region.create_region_by_id("1020").looking_state
-        self.assertEqual(original_looking_state, not new_looking_state)
+        os.remove(f"{self.data_path}/test_map.png")
+        mysec_map.prompt_region_ids_for_looking_state_change = functionTemp
 
     def test_prompt(self):
         mysec_map.input = lambda x: ""
@@ -44,11 +38,6 @@ class TestInitiation(unittest.TestCase):
             mysec_map.prompt_region_ids_for_looking_state_change(),
             [1010, 2020])
         mysec_map.input = input
-
-    def test_get_regions_details(self):
-        self.assertEqual(
-            len(mysec_map.get_regionIds_regionNames_and_lookingBools(self.c)),
-            40)
 
     def test_change_fill_color(self):
         xml_path_raw = '''
