20db46db2a475a24551bb2b13419af78d0b2399b
Martin <martinfrassek@gmx.de>
2021-01-14 22:57:34 +0100

    Write test_region_mutability()

--- tests/test_region.py
+++ tests/test_region.py
@@ -25,3 +25,34 @@ class TestInitiation(unittest.TestCase):
         self.assertEqual(reg.mail_name, "ms")
         self.assertEqual(reg.magazine_name, "mo")
         self.assertEqual(reg.looking_state, 1)
+
+    def test_region_mutability(self):
+        reg = Region(
+            id_=1, name="Münster", mail_name="ms",
+            magazine_name="mo", looking_state=True)
+        reg.looking_state = 0
+        self.assertEqual(reg.looking_state, 0)
+        try:
+            reg.id = 2
+        except (AttributeError):
+            pass
+        else:
+            self.assertTrue(False, "Setting id should fail")
+        try:
+            reg.name = "New"
+        except (AttributeError):
+            pass
+        else:
+            self.assertTrue(False, "Setting name should fail")
+        try:
+            reg.mail_name = "New"
+        except (AttributeError):
+            pass
+        else:
+            self.assertTrue(False, "Setting mail_name should fail")
+        try:
+            reg.magazine_name = "New"
+        except (AttributeError):
+            pass
+        else:
+            self.assertTrue(False, "Setting magazine_name should fail")
