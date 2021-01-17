f3ef179a4c61ab87fe923785259fb67a57561184
Martin <martinfrassek@gmx.de>
2021-01-17 18:58:32 +0100

    Remove dead code get_general_volunteer_details(), get_position_details(), and get_data_type_from_column_name()

--- utils/helper.py
+++ utils/helper.py
@@ -18,24 +18,3 @@ def uncommited_disconnect_from_db(conn):
 
 def get_relative_path_to_script():
     return os.path.dirname(os.path.abspath(__file__))
-
-
-def get_general_volunteer_details(c, volunteerName):
-    c.execute("SELECT * FROM volunteers WHERE volunteerName = '{}'"
-              .format(volunteerName))
-    return c.fetchall()[0]
-
-
-def get_position_details(c, volunteerName, position):
-    c.execute("SELECT * FROM {} WHERE volunteerName = '{}'"
-              .format(position, volunteerName))
-    return c.fetchall()[0]
-
-
-def get_data_type_from_column_name(column_name):
-    if column_name.endswith("Name"):
-        return "name"
-    elif column_name.endswith("Date"):
-        return "date"
-    elif column_name.endswith("gender"):
-        return "gender"
