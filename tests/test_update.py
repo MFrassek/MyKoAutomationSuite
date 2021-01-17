b2e3552b572e44d4fe2440ce3a2ec986866c8237
Martin <martinfrassek@gmx.de>
2021-01-17 12:13:57 +0100

    Revise test_update.py accordingly to the changes in volunteer_add_position.py

--- tests/test_update.py
+++ tests/test_update.py
@@ -1,8 +1,9 @@
 import unittest
 import os
-from utils import volunteer_update_details
+from utils import volunteer_update_position
 from utils import volunteer_add
-from utils import helper
+from utils import init_db
+from _pytest.monkeypatch import MonkeyPatch
 
 
 class TestInitiation(unittest.TestCase):
@@ -10,50 +11,24 @@ class TestInitiation(unittest.TestCase):
         super(TestInitiation, self).__init__(*args, **kwargs)
         self.data_path = "{}/test_data".format(
             os.path.dirname(os.path.abspath(__file__)))
-        self.db_name = "tests/Test.db"
+        self.db_name = "tests/Test_update.db"
 
     def setUp(self):
-        self.conn, self.c = helper.connect_to_db(self.db_name)
+        init_db.init_db(self.data_path, self.db_name)
+        self.monkeypatch = MonkeyPatch()
+        generator = (ele for ele in ["Münster", "2020-01-01", "", ""])
+        self.monkeypatch.setattr(
+            "volunteer_update_position.Position.db_name", self.db_name)
+        self.monkeypatch.setattr("builtins.input", lambda x: next(generator))
+        volunteer_add.add_new_position("MYSec", "Test Person")
 
     def tearDown(self):
-        volunteer_update_details.input = input
-        helper.uncommited_disconnect_from_db(self.conn)
+        self.monkeypatch.undo()
 
-    def test_get_all_positions(self):
-        all_positions = \
-            volunteer_update_details.get_all_positions(self.c, "Test Name")
-        self.assertEqual(all_positions, ["mysecs"])
-
-    def test_update_volunteer_details(self):
-        generator = (ele for ele in ["f", "1990-04-05"])
-        volunteer_update_details.input = lambda x: next(generator)
-        volunteer_update_details.update_volunteer_details(self.c, "Test Name")
-        self.c.execute("""SELECT gender, birthDate FROM volunteers
-                          WHERE volunteerName = 'Test Name'""")
-        self.assertEqual(self.c.fetchall()[0], ("f", "1990-04-05"))
-
-    def test_update_position_details(self):
-        generator = (ele for ele in ["Kiel", "2019-03-02", "2020-03-02"])
-        volunteer_update_details.input = lambda x: next(generator)
-        volunteer_update_details.update_position_details(
-            self.c, "Test Name", "mysecs")
-        self.c.execute("""SELECT regionName, startDate, endDate FROM mysecs
-                          WHERE volunteerName = 'Test Name'""")
-        self.assertEqual(
-            self.c.fetchall()[0], ("Kiel", "2019-03-02", "2020-03-02"))
-
-    def test_update_value_in_table_without_input(self):
-        volunteer_update_details.input = lambda x: ""
-        volunteer_update_details.update_value_in_table(
-            self.c, "Test Name", "mysecs", "startDate", "")
-        self.c.execute("""SELECT startDate FROM mysecs
-                          WHERE volunteerName = 'Test Name'""")
-        self.assertEqual(self.c.fetchall()[0], ("2020-10-10",))
-
-    def test_update_value_in_table_with_input(self):
-        volunteer_update_details.input = lambda x: "2020-11-11"
-        volunteer_update_details.update_value_in_table(
-            self.c, "Test Name", "mysecs", "endDate", "")
-        self.c.execute("""SELECT endDate FROM mysecs
-                          WHERE volunteerName = 'Test Name'""")
-        self.assertEqual(self.c.fetchall()[0], ("2020-11-11",))
+    def test_update_position(self):
+        generator = (ele for ele in ["Test Person", "2021-01-17"])
+        self.monkeypatch.setattr("builtins.input", lambda x: next(generator))
+        volunteer_update_position.update_position()
+        position = volunteer_update_position\
+            .Position.create_all_held_positions("Test Person")[0]
+        self.assertEqual(position.end_date, "2021-01-17")
