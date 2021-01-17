e6798c35c5f41b04d0ef8bfc992969a01e83f68f
Martin <martinfrassek@gmx.de>
2021-01-17 11:48:42 +0100

    Revise volunteer_update_details.py and decouple the updating process from the database

--- utils/volunteer_update_details.py
+++ utils/volunteer_update_details.py
@@ -1,62 +1,14 @@
-from helper import connect_to_db, disconnect_from_db,\
-    get_general_volunteer_details, get_position_details, \
-    get_data_type_from_column_name, string_follows_input_pattern
+from userPrompt import UserPrompt
+from position import Position
 
 
-def update_details(db_name):
-    conn, c = connect_to_db(db_name)
-    volunteerName = input("Name:\n")
-    all_positions = get_all_positions(c, volunteerName)
-    if all_positions:
-        update_volunteer_details(c, volunteerName)
-        for position in all_positions:
-            print(position)
-            update_position_details(c, volunteerName, position)
-    else:
-        print("No positions found for this name")
-    disconnect_from_db(conn)
-
-
-def get_all_positions(c, volunteerName):
-    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    all_positions = []
-    for table in c.fetchall():
-        c.execute("PRAGMA table_info({})".format(table[0]))
-        if c.fetchall()[0][1] == "volunteerName" and table[0] != "volunteers":
-            c.execute("SELECT * FROM {} WHERE volunteerName = '{}';"
-                      .format(table[0], volunteerName))
-            if len(c.fetchall()) >= 1:
-                all_positions.append(table[0])
-    return all_positions
-
-
-def update_volunteer_details(c, volunteerName):
-    _, gender, birthDate = get_general_volunteer_details(c, volunteerName)
-    update_value_in_table(c, volunteerName, "volunteers", "gender", gender)
-    update_value_in_table(
-        c, volunteerName, "volunteers", "birthDate", birthDate)
-
-
-def update_position_details(c, volunteerName, position):
-    _, regionName, startDate, endDate = get_position_details(
-        c, volunteerName, position)
-    update_value_in_table(c, volunteerName, position, "regionName", regionName)
-    update_value_in_table(c, volunteerName, position, "startDate", startDate)
-    update_value_in_table(c, volunteerName, position, "endDate", endDate)
-
-
-def update_value_in_table(c, volunteerName, table, column_name, value):
-    data_type = get_data_type_from_column_name(column_name)
-    while True:
-        new_value = input(
-            f"Current {column_name}: {value}\nNew {column_name}: ")
-        if string_follows_input_pattern(new_value, data_type):
-            break
-    if new_value:
-        c.execute(f"""UPDATE {table}
-            SET {column_name} = '{new_value}'
-            WHERE volunteerName = '{volunteerName}';""")
+def update_position():
+    position = UserPrompt.select_from_options(
+        Position.create_all_held_positions(UserPrompt.get_volunteer_name()))
+    UserPrompt.inform_about_update_start()
+    position.end_date = UserPrompt.get_end_date()
+    position.update_in_db()
 
 
 if __name__ == '__main__':
-    update_details("MY-Ko.db")
+    update_position()
