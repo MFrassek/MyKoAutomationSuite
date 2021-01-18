7dc897b5d09987f4981cd54fc0d44251006a03f1
Martin <martinfrassek@gmx.de>
2021-01-18 18:44:04 +0100

    Add tests for Volunteer class similar to the ones for the Region class

---
+++ tests/test_volunteer.py
@@ -0,0 +1,81 @@
+import unittest
+import os
+from utils import person
+from person import Volunteer
+from utils import position
+from position import Position
+from _pytest.monkeypatch import MonkeyPatch
+from utils import init_db
+
+
+class TestVolunteer(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestVolunteer, self).__init__(*args, **kwargs)
+        self.data_path = "{}/test_data".format(
+            os.path.dirname(os.path.abspath(__file__)))
+        self.db_name = "tests/Test.db"
+
+    def setUp(self):
+        self.monkeypatch = MonkeyPatch()
+        self.monkeypatch.setattr(
+            "position.Position.db_name", self.db_name)
+        self.monkeypatch.setattr(
+            "person.Volunteer.db_name", self.db_name)
+        init_db.init_db(self.data_path, self.db_name)
+        Position("MYSec", "Kiel", "Test Person", "2020-04-04", "").add_to_db()
+        Volunteer("Test Person", "u", "1994-07-05").add_to_db()
+
+    def tearDown(self):
+        self.monkeypatch.undo()
+
+    def test_volunteer_accessibility(self):
+        vol = Volunteer(
+            name="Test Person", gender="u",
+            birth_date="1994-07-05")
+        self.assertEqual(vol.name, "Test Person")
+        self.assertEqual(vol.gender, "u")
+        self.assertEqual(vol.birth_date, "1994-07-05")
+        self.assertEqual(vol.positions[0].title, "MYSec")
+        self.assertEqual(vol.positions[0].held_by, "Test Person")
+        self.assertEqual(vol.positions[0].region, "Kiel")
+        self.assertEqual(vol.positions[0].start_date, "2020-04-04")
+        self.assertEqual(vol.positions[0].end_date, "")
+
+    def test_volunteer_mutability(self):
+        vol = Volunteer(
+            name="Test Person", gender="u",
+            birth_date="1994-07-05")
+        with self.assertRaises(AttributeError):
+            vol.name = "New Name"
+        with self.assertRaises(AttributeError):
+            vol.gender = "New"
+        with self.assertRaises(AttributeError):
+            vol.birth_date = "0001-01-01"
+        with self.assertRaises(AttributeError):
+            vol.positions = ("New Position")
+
+    def test_volunteer_equal(self):
+        self.assertEqual(
+            Volunteer(name="Test Person", gender="u", birth_date="1994-07-05"),
+            Volunteer(name="Test Person", gender="u", birth_date="1994-07-05"))
+        self.assertNotEqual(
+            Volunteer(name="Test Person", gender="u", birth_date="1994-07-05"),
+            Volunteer(name="Test Verson", gender="u", birth_date="1994-07-05"))
+
+    def test_volunteer_hash(self):
+        self.assertEqual(
+            hash(Volunteer(name="Test Person", gender="u", birth_date="1994-07-05")),
+            hash(Volunteer(name="Test Person", gender="u", birth_date="1994-07-05")))
+        self.assertNotEqual(
+            hash(Volunteer(name="Test Person", gender="u", birth_date="1994-07-05")),
+            hash(Volunteer(name="Test Verson", gender="u", birth_date="1994-07-05")))
+
+    def test_volunteer_factory_methods(self):
+        self.assertEqual(len(Volunteer.create_all()), 1)
+        self.assertEqual(len(Volunteer.create_all_fitting_data(
+            [["name", "=", "Test Person"]])), 1)
+
+    def test_get_details_fitting_data(self):
+        self.assertEqual(
+            Volunteer.get_details_fitting_data([["name", "=", "Test Person"]]),
+            [("Test Person", "u", "1994-07-05")])
