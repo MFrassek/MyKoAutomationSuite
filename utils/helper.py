9df0ef366adc864650314cc387a6e185847181d2
Martin <martinfrassek@gmx.de>
2021-01-26 18:50:51 +0100

    Remove dead code: connect_to_db(), disconnect_from_db(), and uncommited_disconnect_from_db() as these are now handled by the DatabaseConnection class

--- utils/helper.py
+++ utils/helper.py
@@ -1,19 +1,4 @@
-import sqlite3
 import os
-
-
-def connect_to_db(db_name):
-    conn = sqlite3.connect(db_name)
-    return conn, conn.cursor()
-
-
-def disconnect_from_db(conn):
-    conn.commit()
-    conn.close()
-
-
-def uncommited_disconnect_from_db(conn):
-    conn.close()
 
 
 def get_relative_path_to_script():
