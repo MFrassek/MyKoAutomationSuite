d6c146a5159080d20377afbf5ba990678a10bb74
Martin <martinfrassek@gmx.de>
2021-01-26 22:58:34 +0100

    Extract get_today_at_midnight(), get_birthday_this_year(), and get_days_until_birthday() from print_coming_birthdays()

--- utils/birthday_reminder.py
+++ utils/birthday_reminder.py
@@ -3,14 +3,25 @@ from datetime import datetime
 
 
 def print_coming_birthdays():
-    today = datetime.now()\
-        .replace(hour=0, minute=0, second=0, microsecond=0)
+    today = get_today_at_midnight()
     for volunteer in Volunteer.create_all():
-        birthday = datetime.strptime(volunteer.birth_date, "%Y-%m-%d")\
-            .replace(year=today.year)
-        days_until_birthday = (birthday - today).days
+        birthday_this_year = get_birthday_this_year(volunteer, today)
+        days_until_birthday = get_days_until_birthday(birthday_this_year, today)
         if volunteer.is_active() and 0 <= days_until_birthday <= 21:
             print(f"{volunteer.name}'s birthday is in {days_until_birthday} days")
+
+
+def get_today_at_midnight():
+    return datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def get_birthday_this_year(volunteer, today):
+    return datetime.strptime(volunteer.birth_date, "%Y-%m-%d")\
+            .replace(year=today.year)
+
+
+def get_days_until_birthday(birthday_this_year, today):
+    return (birthday_this_year - today).days
 
 
 if __name__ == "__main__":
