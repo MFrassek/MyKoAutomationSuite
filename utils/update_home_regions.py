a0cb3321b7fcaff6feb06f04fdd83478aaafafc3
Martin <martinfrassek@gmx.de>
2021-01-31 22:48:46 +0100

    Add functionality to read out data received from db.mensa and print which volunteers moved
    Extract first name, last name and zip code from the data table.
    Use RegionFinder on the zip code to find the current region of the volunteer.
    Use the name to create the current version of the volunteer from the DB.
    If the volunteer exists, is active and has a different home region stored
    then the one found on db.mensa.de print the name and the new region.

--- utils/update_home_regions.py
+++ utils/update_home_regions.py
@@ -1,6 +1,8 @@
 import json
 import requests
 from bs4 import BeautifulSoup
+from regionFinder import RegionFinder
+from person import Volunteer
 
 
 def update_volunteer_home_regions():
@@ -17,6 +19,26 @@ def update_volunteer_home_regions():
         "lastchoice": "8"}
     req = requests.get(url, params=params, data=data)
     soup = BeautifulSoup(req.text, "html5lib")
+
+    person_table = soup("table")[15]("tr")[1:-1]
+    for row in person_table:
+        row_details = row("td")[3:]
+        if row_details and row_details_are_valid(row_details):
+            last_name = row_details[0].text.strip()
+            first_name = row_details[1].text.strip()
+            zip_code = int(row_details[3].text.strip() or -1)
+            region = RegionFinder.find_region_belonging_to_zip_code(zip_code)
+            name = first_name + " " + last_name
+            vol = Volunteer.create_by_name(name)
+            if vol and vol.is_active() and vol.home_region != region:
+                print(name + " " + region)
+
+
+def row_details_are_valid(row_details: str):
+    if row_details[0].text \
+            and not row_details[0].text.strip().startswith("Vertreter"):
+        return True
+    return False
 
 
 if __name__ == "__main__":
